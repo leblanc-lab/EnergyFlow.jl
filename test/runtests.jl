ebefc53f4916c119fe32119f892ca752d38e2cf4
Matt LeBlanc <matt.leblanc@cern.ch>
2026-06-22 14:09:32 -0400

    Removing duplicate TEST_DEBUG definition

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,6 @@ push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
 using EnergyFlow
 include("test_helpers.jl")
 
-const TEST_DEBUG = lowercase(get(ENV, "TEST_DEBUG", "false")) in ("1", "true", "yes", "on")
-test_log(args...) = TEST_DEBUG ? println(args...) : nothing
-
 @testset "EnergyFlow" begin
     include("test_distances.jl")
     include("test_emd.jl")
